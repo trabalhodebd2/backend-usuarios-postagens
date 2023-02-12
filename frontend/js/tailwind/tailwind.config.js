9aba37b7521b09b5537fe70729adf6e3b5dd7d63
lyzzandro <lyzzandro91@gmail.com>
2023-02-12 10:43:16 -0300

    creating login and cadastre pages

--- frontend/js/tailwind/tailwind.config.js
+++ frontend/js/tailwind/tailwind.config.js
@@ -2,10 +2,12 @@ tailwind.config = {
     theme: {
         extend: {
             colors: {
-                primary: '#F5F5F5',
-                secondary: '#00000080',
-                tertiary: '#D90429',
+                primary: "#EDF2F4",
+                secondary: "#00000080",
+                tertiary: "#EF233C",
+                tertiary_hover: "#D90429",
+                gray: "#8D99AE",
             },
-        }
-    }
-}
+        },
+    },
+};
