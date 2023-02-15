299fa219a4ee047a730667117c2ce3ddb73ccf1f
Pedro Júnior <pedrojuniorifpb@gmail.com>
2023-02-15 02:35:53 -0300

    Modificando a escrita do aquivo de rotas

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 // const isAuthenticated = require("../middlewares/isAuthenticated.js");
 
-// const {
-// 	userObjectExists,
-// 	isSameUserPermission,
-// } = require("../middlewares/userPermission.js");
+const {
+	userObjectExists,
+	isSameUserPermission,
+} = require("../middlewares/userPermission.js");
 
-// const postObjectExists = require("../middlewares/postPermission.js");
+const postObjectExists = require("../middlewares/postPermission.js");
 
 const postController = require("../controllers/postController.js");
 
@@ -30,37 +30,34 @@ const postController = require("../controllers/postController.js");
 // 	postController.createUserPost
 // );
 
-// router.get(
-// 	"/users/:userId/posts/:postId",
-// 	isAuthenticated,
-// 	userObjectExists,
-// 	postObjectExists,
-// 	isSameUserPermission,
-// 	postController.getUserPostById
-// );
-
-// router.put(
-// 	"/users/:userId/posts/:postId",
-// 	isAuthenticated,
-// 	userObjectExists,
-// 	postObjectExists,
-// 	isSameUserPermission,
-// 	postController.updateUserPost
-// );
-
-// router.delete(
-// 	"/users/:userId/posts/:postId",
-// 	isAuthenticated,
-// 	userObjectExists,
-// 	postObjectExists,
-// 	isSameUserPermission,
-// 	postController.deleteUserPost
-// );
-
 router.get("/users/:id/posts", postController.getUserPosts)
 router.post("/users/:id/posts", postController.createUserPost)
-router.get("/users/:id/posts/:postId", postController.getUserPostById)
-router.put("/users/:id/posts/:postId", postController.updateUserPost)
-router.delete("/users/:id/posts/:postId", postController.deleteUserPost)
+
+router.get(
+	"/users/:userId/posts/:postId",
+	// isAuthenticated,
+	userObjectExists,
+	postObjectExists,
+	// isSameUserPermission,
+	postController.getUserPostById
+);
+
+router.put(
+	"/users/:userId/posts/:postId",
+	// isAuthenticated,
+	userObjectExists,
+	postObjectExists,
+	// isSameUserPermission,
+	postController.updateUserPost
+);
+
+router.delete(
+	"/users/:userId/posts/:postId",
+	// isAuthenticated,
+	userObjectExists,
+	postObjectExists,
+	// isSameUserPermission,
+	postController.deleteUserPost
+);
 
 module.exports = router;
